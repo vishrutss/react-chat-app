90ac84f39a825433c978430143c7f10312b10645
vishrutss <vishrutss@gmail.com>
2023-07-28 12:20:24 +0530

    Added code to retrieve user details from cookies

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const User = require("./models/User");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -12,6 +13,7 @@ const jwtSecret = process.env.JWT_SECRET;
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
@@ -22,18 +24,36 @@ app.get("/test", (req, res) => {
   res.json("test ok");
 });
 
-app.post('/register', async (req, res) => {
+app.get("/profile", (req, res) => {
+  const token = req.cookies?.token;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, (err, userData) => {
+      if (err) throw err;
+      res.json(userData);
+    });
+  } else {
+    res.status(401).json("no token");
+  }
+});
+
+app.post("/register", async (req, res) => {
   const { username, password } = req.body;
   try {
     const createdUser = await User.create({ username, password });
-    jwt.sign({ userId: createdUser._id }, jwtSecret,
+    jwt.sign(
+      { userId: createdUser._id, username },
+      jwtSecret,
       {},
       (err, token) => {
         if (err) throw err;
-        res.cookie('token', token).status(201).json({
-          id: createdUser._id,
-        });
-      });
+        res
+          .cookie("token", token, { sameSite: "none", secure: true })
+          .status(201)
+          .json({
+            id: createdUser._id,
+          });
+      }
+    );
   } catch (error) {
     if (error) throw error;
     res.status(500).json("error");
