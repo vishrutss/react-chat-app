420ad6a56be5a2bea86fef3ea37568fdc5a3fc8d
vishrutss <vishrutss@gmail.com>
2023-07-29 17:30:02 +0530

    Added login functionality

--- api/index.js
+++ api/index.js
@@ -40,7 +40,23 @@ app.get("/profile", (req, res) => {
 
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  const foundUser = await User.find({ username });
+  const foundUser = await User.findOne({ username });
+  if (foundUser) {
+    const passOk = bcrypt.compareSync(password, foundUser.password);
+    if (passOk) {
+      jwt.sign(
+        { userId: foundUser._id, username },
+        jwtSecret,
+        {},
+        (err, token) => {
+          if (err) throw err;
+          res.cookie("token", token).json({
+            id: foundUser._id,
+          });
+        }
+      );
+    }
+  }
 });
 
 app.post("/register", async (req, res) => {
